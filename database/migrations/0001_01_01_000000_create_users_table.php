30958d9ff9b4ea5c3213acf91c65a3e26c222dfb
mamantube <firmanhiday.id007@gmail.com>
2025-07-14 17:13:46 +0700

    set up users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('user_name')->unique();
+            $table->string('phone')->unique();
+            $table->string("role")->default('user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp("deleted_at")->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

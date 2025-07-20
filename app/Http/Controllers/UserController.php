f426ef927a8f7e29df5f4740ca7e01634cb76554
mamantube <firmanhiday.id007@gmail.com>
2025-07-20 11:16:29 +0700

    add login function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,4 +36,26 @@ class UserController extends Controller
             return response()->json(["error" => $e->getMessage()], 400);
         }
     }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                "email" => "required|email",
+                "password" => "required|string"
+            ]);
+
+            $user = User::where("email", $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json(["message" => "Email atau password salah"], 401);
+            }
+
+            $token = $user->createToken("auth_token")->plainTextToken;
+
+            return response()->json(["message" => "Login berhasil!", "user" => $user, "token" => $token], 200);
+        } catch (Exception $e) {
+            return response()->json(["error" => $e->getMessage()], 400);
+        }
+    }
 }

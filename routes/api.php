77bf16aab0eec40162f0f060ee85ac9f66d80054
mamantube <firmanhiday.id007@gmail.com>
2025-07-28 22:11:00 +0700

    add create produk, get all produk and detail product by id route api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 Route::post("register", [UserController::class, "register"]);
 Route::post("login", [UserController::class, "login"]);
@@ -10,3 +11,7 @@ Route::get("all-users", [UserController::class, "getAllUsers"]);
 Route::get("user/{id}", [UserController::class, "getUserById"]);
 Route::post("user/{id}", [UserController::class, "updateUser"]);
 Route::delete("user/{id}", [UserController::class, "deleteUser"]);
+
+Route::post("/add-product", [ProductController::class, "addProduct"]);
+Route::get("/products/{id}", [ProductController::class, "productDetail"]);
+Route::get("products", [ProductController::class, "getAllProduct"]);

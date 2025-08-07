1fd641815eaa6b24ba8909ac399ebaa39b588612
mamantube <firmanhiday.id007@gmail.com>
2025-08-07 16:30:31 +0700

    implementation asuth sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartItemController;
 
 Route::post("register", [UserController::class, "register"]);
 Route::post("login", [UserController::class, "login"]);
-Route::get("all-users", [UserController::class, "getAllUsers"]);
-Route::get("user/{id}", [UserController::class, "getUserById"]);
-Route::post("user/{id}", [UserController::class, "updateUser"]);
-Route::delete("user/{id}", [UserController::class, "deleteUser"]);
 
-Route::post("/add-product", [ProductController::class, "addProduct"]);
 Route::get("/products/{id}", [ProductController::class, "productDetail"]);
 Route::get("products", [ProductController::class, "getAllProduct"]);
-Route::post("products/{id}", [ProductController::class, "updateProduct"]);
-Route::delete("products/{id}", [ProductController::class, "deleteProduct"]);
+
+Route::middleware(["auth:sanctum"])->group( function() {
+    Route::get("all-users", [UserController::class, "getAllUsers"]);
+    Route::get("user/{id}", [UserController::class, "getUserById"]);
+    Route::post("user/{id}", [UserController::class, "updateUser"]);
+    Route::delete("user/{id}", [UserController::class, "deleteUser"]);
+    
+    Route::prefix("produccts")->group( function() {
+        Route::post("/add-product", [ProductController::class, "addProduct"]);
+        Route::post("/{id}", [ProductController::class, "updateProduct"]);
+        Route::delete("/{id}", [ProductController::class, "deleteProduct"]);      
+    });
+    Route::post("carts", [CartItemController::class, "addToCart"]);
+});

bc99ba7a88c26ae27f9828e5f2b06c1eebb05679
mamantube <firmanhiday.id007@gmail.com>
2025-08-14 16:16:27 +0700

    import bootstrap

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,6 +5,8 @@ import Beranda from "./pages/Beranda";
 import "antd/dist/reset.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const App = () => {
     return (

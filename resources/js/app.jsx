14cdbe63afa6b941cefa55a2d95e6ec74fc3f27f
mamantube <firmanhiday.id007@gmail.com>
2025-08-14 15:27:50 +0700

    add store and provider

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Beranda from "./pages/Beranda";
+import "antd/dist/reset.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" Component={Landing} />
-            </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Beranda />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     );
 }
 

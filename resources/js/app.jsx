6f7c4383e2c48fca5b54b9a3fddff2593d002d6c
mamantube <firmanhiday.id007@gmail.com>
2025-08-14 17:56:02 +0700

    tidy up and remove ant design

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Beranda from "./pages/Beranda";
-// import "antd/dist/reset.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Landing from "./layout/Landing";
 
 const App = () => {
     return (
         <Provider store={store}>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Beranda />} />
+                    <Route element={<Landing />}>
+                        <Route index path="/" element={<Beranda />} />
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </Provider>
